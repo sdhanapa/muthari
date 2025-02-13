d0bc2fd6c76f960431331ba2535006da1e156dd1
sdhanapa <34435689+sdhanapa@users.noreply.github.com>
2025-02-13 20:23:09 +0530

    Update fptr_taking_void_ptr_arg.c

--- Function_pointers/fptr_taking_void_ptr_arg.c
+++ Function_pointers/fptr_taking_void_ptr_arg.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Write a C program to demonstrate function Pointer with Void Pointer Argument
+
 void getInt(void *data) {
     int *info = (int*)data; // typecasting the void pointer to specific data type
     printf("Integer data: %d",*info);
